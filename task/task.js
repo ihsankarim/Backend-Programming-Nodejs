6aac817dcf9506ee82b50e423da1ece0a99234eb
ihsankarim <ihsanalkarim303@gmail.com>
2022-12-05 13:41:54 +0700

    Refactor callback ke Promise atau Async Await

---
+++ task/task.js
@@ -0,0 +1,36 @@
+/**
+ * Fungsi untuk menampilkan hasil download
+ * @param {string} result - Nama file yang didownload
+ */
+const showDownload = (result) => {
+  console.log("Download selesai");
+  console.log(`Hasil download: ${result}`);
+};
+
+/**
+ * Fungsi untuk download file
+ * @param {function} callback - Function callback show 
+ */
+const download = (callShowDownload) => {
+  return new Promise((resolve) => {
+    const result = "Windows-10.exe";
+    setTimeout(() => {
+      resolve(callShowDownload(result));
+    }, 3000);
+  });
+};
+
+const main = async () => { 
+    show = await download(showDownload);
+    return show;
+};
+
+main();
+
+
+/**
+ * TODO:
+ * - Refactor callback ke Promise atau Async Await
+ * - Refactor function ke ES6 Arrow Function
+ * - Refactor string ke ES6 Template Literals
+ */
